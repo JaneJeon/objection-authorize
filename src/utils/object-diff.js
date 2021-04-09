fdc5cc900e330b4fe77968c93a64e153c9f4561a
Jane Jeon <me@janejeon.dev>
2021-04-08 20:37:21 -0400

    add special date handling

--- src/utils/object-diff.js
+++ src/utils/object-diff.js
@@ -4,6 +4,8 @@
 // or has a difference value from A
 const isEmpty = require('lodash/isEmpty')
 const isObject = require('./is-object')
+const isDate = require('lodash/isDate')
+const isEqual = require('lodash/isEqual')
 
 function objectDiff(A, B) {
   // if types differ, or are "scalars" (including arrays), ignore it
@@ -13,6 +15,11 @@ function objectDiff(A, B) {
     if (isObject(B[fieldB])) {
       const subResult = objectDiff(A[fieldB], B[fieldB])
       if (!isEmpty(subResult)) result[fieldB] = subResult
+    } else if (isDate(A) || isDate(B)) {
+      // special case handling for date objects/strings, for two reasons:
+      // 1. Javascript Dates aren't serializable into JSON, so they often come as strings
+      // 2. Even if you DID pass two dates, you can't directly compare them using ==/===
+      if (!isEqual(new Date(A), new Date(B))) result[fieldB] = B[fieldB]
     } else {
       // it's a "scalar", so compare B's field as-is.
       // For "object-like" stuff like arrays, it will still be "different".
